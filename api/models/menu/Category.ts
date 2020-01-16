ac15d5fac920453786616f7d3b6b7b193a8e1785
Amjad Shaaban <theamjad@outlook.com>
2020-01-15 23:35:05 -0500

    fix category schema definitions
    declares references as array

--- api/models/menu/Category.ts
+++ api/models/menu/Category.ts
@@ -4,7 +4,7 @@ import { ICategory } from '../../interfaces/interfaces';
 const CategorySchema = new Schema({
   name: { type: String, required: true, unique: true },
   img: { type: String, default: '' },
-  items: { type: Schema.Types.ObjectId, ref: 'CategoryItem' }
+  items: [{ type: Schema.Types.ObjectId, ref: 'CategoryItem' }]
 });
 
 export default mongoose.model<ICategory>('Category', CategorySchema);
--- api/models/menu/CategoryItem.ts
+++ api/models/menu/CategoryItem.ts
@@ -3,7 +3,7 @@ import { ICategoryItem } from '../../interfaces/interfaces';
 
 const CategoryItemSchema = new Schema({
   name: { type: String, required: true, unique: true },
-  ingredients: { type: Schema.Types.ObjectId, ref: 'Ingredient' }
+  ingredients: [{ type: Schema.Types.ObjectId, ref: 'Ingredient' }]
 });
 
 export default mongoose.model<ICategoryItem>(
